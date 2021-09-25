79b693eb565e805c8e9b67b92355a64452d5054d
DerickLFAndrade <derickspy15@gmail.com>
2021-09-25 02:48:38 -0300

    Design desktop

---
+++ JavaScript.js
@@ -0,0 +1,131 @@
+/*var inputs = document.querySelectorAll('.imps')
+var valor = document.getElementById('valores')
+var result = document.getElementById('resultado')
+var input1 = document.getElementById("imps1")
+
+ 
+    
+var valorSoma1
+var valorSoma2
+  
+       function num7() {
+
+           
+            result.innerHTML += String(inputs[0].value)
+            valorSoma1 = Number(result.textContent)
+            console.log(valorSoma1)
+       }
+
+       function num8() {
+
+           result.innerHTML += String(inputs[1].value)
+           console.log(result)
+       }
+       function num9() {
+
+           result.innerHTML += inputs[2].value
+       }
+
+       function num4() {
+
+           result.innerHTML += inputs[4].value
+       }
+       function num5() {
+
+           result.innerHTML += inputs[5].value
+       }
+       function num6() {
+
+           result.innerHTML += inputs[6].value
+       }
+       function num1() {
+
+           result.innerHTML += inputs[8].value
+       }
+       function num2() {
+
+           result.innerHTML += inputs[9].value
+       }
+       function num3() {
+
+           result.innerHTML += inputs[10].value
+       }
+       function num0() {
+
+           result.innerHTML += inputs[13].value
+       }
+
+
+       function somar() {
+           
+         
+           if(valorSoma1 <6 ) {
+           valorSoma2 = Number(result.textContent)
+               console.log(valorSoma2)
+           }
+           
+           
+           
+         
+           
+       }
+       function subtrair() {
+
+           result.innerHTML += inputs[11].value
+       }
+       function dividir() {
+
+           result.innerHTML += inputs[14].value
+       }
+       function multiplicar() {
+
+           result.innerHTML += inputs[15].value
+       }
+       function resetar() {
+
+           result.innerHTML += inputs[16].value
+       }
+       function resultado() {
+
+         if (valorSoma1 && valorSoma2) {
+           console.log(valorSoma1)
+             console.log(valorSoma2)
+         }
+       }
+
+       function porcentagem() {
+
+           result.innerHTML += inputs[18].value
+       }
+
+       function ponto() {
+
+           result.innerHTML += inputs[12].value
+       }
+       function deleta() {
+
+           //pos4
+       }
+
+       inputs[0].addEventListener('click', num7)
+       inputs[1].addEventListener('click', num8)
+       inputs[2].addEventListener('click', num9)
+       inputs[3].addEventListener('click', deleta)
+       inputs[4].addEventListener('click', num4)
+       inputs[5].addEventListener('click', num5)
+       inputs[6].addEventListener('click', num6)
+       inputs[7].addEventListener('click', somar)
+       inputs[8].addEventListener('click', num1)
+       inputs[9].addEventListener('click', num2)
+       inputs[10].addEventListener('click', num3)
+       inputs[11].addEventListener('click', subtrair)
+       inputs[12].addEventListener('click', ponto)
+       inputs[13].addEventListener('click', num0)
+       inputs[14].addEventListener('click', dividir)
+       inputs[15].addEventListener('click', multiplicar)
+       inputs[16].addEventListener('click', resetar)
+       inputs[18].addEventListener('click', porcentagem)
+       inputs[17].addEventListener('click', resultado)
+     
+   APRENDENDO...
+*/ 
--- README.md
+++ README.md
@@ -13,16 +13,41 @@
 - #### Pocentagem.
 
 
+**Será desenvolvido um modelo para mobile e outro para desktop**
 
-**Será desenvolvido um modelo para mobile e outro para desktop**.
+**Desktop:**
+
+![desktop-preview](C:\Users\Dev Js\OneDrive\Área de Trabalho\Front End Mentor\CALCULADORA COMUM\Desafio\design\desktop-preview.jpg)
+
+
+
+**Mobile**:
+
+
+
+
+
+
+
+![mobile-design-theme-1](C:\Users\Dev Js\OneDrive\Área de Trabalho\Front End Mentor\CALCULADORA COMUM\Desafio\design\mobile-design-theme-1.jpg)
+
+
+
+
+
+
+
+
+
+
 
 
 
 | TASKS                                    |    PROGRESSO     | *REQUERIDO PARA* | *ENTREGUE EM* | *RELEVÂNCIA* 1/10 |
 | :--------------------------------------- | :--------------: | :--------------: | :-----------: | :---------------: |
-| - Criar esqueleto da pasta main e criar arquivos: **.hml**, **.js** , .**css**, **.md** ; |  **Concluído**   |    24/09/2021    |  24/09/2021   |       10/10       |
-| - Criar esboço da página index.html;     | **Em andamento** |    24/09/2021    |               |       8/10        |
-| - Criar um repositório e upar a pasta do projeto no GitHub; | **Em andamento** |                  |               |                   |
+| - Criar esqueleto da pasta main e criar arquivos: **.hml**, **.js** , .**css**, **.md** ; |  **Concluído**   |    24/09/2021    |  24/09/2021   |       8/10        |
+| - Criar esboço da página index.html;     |  **Concluído**   |    24/09/2021    |  24/09/2021   |       8/10        |
+| - Criar um repositório e upar a pasta do projeto no GitHub; |  **Concluído**   |    24/09/2021    |  24/09/2021   |       10/10       |
 | -                                        | **Em andamento** |                  |               |                   |
 |                                          | **Em andamento** |                  |               |                   |
 |                                          | **Em andamento** |                  |               |                   |
