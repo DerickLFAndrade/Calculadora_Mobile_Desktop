1206f1fb14e14a6ee8e61e63ae79fcabab8a3d97
DerickLFAndrade <derickspy15@gmail.com>
2021-09-25 19:30:19 -0300

    Design principal e 3 opções de temas

--- JavaScript.js
+++ JavaScript.js
@@ -1,131 +1,135 @@
-/*var inputs = document.querySelectorAll('.imps')
-var valor = document.getElementById('valores')
-var result = document.getElementById('resultado')
-var input1 = document.getElementById("imps1")
+var pos1 = document.getElementById('pos1')
+var pos2 = document.getElementById('pos2')
+var pos3 = document.getElementById('pos3')
+var contP = document.getElementById('containerPrincipal')
+var BarraTema1 = document.getElementById("calcEsquerda" )
+var BarraTema2 = document.getElementsByClassName('corLetra')
 
- 
+var pads = document.getElementById("pads")
+var pads1 = document.getElementsByClassName('imps')
+var inps = document.getElementsByClassName('imps')
+
+function tema1() {
+    for (let i = 0; i <= 4; i++) {
+        BarraTema2[i].style.color = 'white'
+
+    }
+    for (i = 0; i <= 2; i++) {
+        inps[i].style.background = 'hsl(0, 0%, 93%)'
+        inps[i].style.color = 'hsl(60, 10%, 19%)'
+    }
+
+    for (i = 4; i <= 15; i++) {
+        inps[i].style.color = 'hsl(60, 10%, 19%)'
+        inps[18].style.color = 'hsl(52, 100%, 62%)'
+        inps[i].style.background = 'hsl(0, 0%, 93%)'
+        inps[3].style.background = 'hsl(225, 21%, 49%)'
+        inps[16].style.background = 'hsl(225, 21%, 49%)'
+        inps[17].style.background = 'hsl(6, 63%, 50%)'
+        inps[18].style.background = 'hsl(0, 0%, 93%)'
+        inps[17].style.color = 'white'
+        inps[18].style.color = 'hsl(60, 10%, 19%)'
+    }
+
+
+    resultado.style.background = 'hsl(224, 36%, 15%)'
+    resultado.style.color = 'white'
     
-var valorSoma1
-var valorSoma2
-  
-       function num7() {
+    pads.style.background = 'hsl(223, 31%, 20%)'  
+    contP.style.background = 'hsl(222, 26%, 31%)'
+    pos1.style.background = 'hsl(6, 63%, 50%)'
+    pos2.style.background = 'hsl(224, 36%, 15%'
+    pos3.style.background = 'hsl(224, 36%, 15%'
+  }
+function tema2() {
+    for (let i = 0; i <= 4; i++) {
+        BarraTema2[i].style.color = 'hsl(60, 10%, 19%)'
 
-           
-            result.innerHTML += String(inputs[0].value)
-            valorSoma1 = Number(result.textContent)
-            console.log(valorSoma1)
-       }
+    }
+    for (i = 0; i <= 2; i++) {
+        inps[i].style.background = 'hsl(0, 0%, 93%)'
+        inps[i].style.color = 'hsl(60, 10%, 19%)'
+    }
 
-       function num8() {
+    for (i = 4; i <= 15; i++) {
+        inps[i].style.color = 'hsl(60, 10%, 19%)'
+        inps[18].style.color = 'hsl(52, 100%, 62%)'
+        inps[i].style.background = 'hsl(0, 0%, 93%)'
+        inps[3].style.background = 'hsl(185, 42%, 37%)'
+        inps[17].style.background = 'hsl(25, 98%, 40%)'
+        inps[16].style.background = 'hsl(185, 42%, 37%)'
+        inps[18].style.background = 'hsl(0, 0%, 93%)'
+        inps[17].style.color = 'white'
+        inps[18].style.color = 'hsl(60, 10%, 19%)'
+    }
+    
+    resultado.style.background = 'white'
+    resultado.style.color = 'black'
+    
+    pads.style.background = 'hsl(0, 5%, 81%)'
+    contP.style.background = 'hsl(0, 0%, 90%)'
+    pos1.style.background = 'hsl(224, 36%, 15%)'
+    pos2.style.background = 'hsl(25, 98%, 50%)'
+    pos3.style.background = 'hsl(224, 36%, 15%)'
+}
+function tema3() {
+    
+    contP.style.background = 'hsl(268, 75%, 9%)'
+    
+    for (let i = 0; i <= 4; i++) {
+        BarraTema2[i].style.color = 'hsl(52, 100%, 62%)'
 
-           result.innerHTML += String(inputs[1].value)
-           console.log(result)
-       }
-       function num9() {
+    }
+    for (i = 0; i <= 2; i++) {
+        inps[i].style.background = 'hsl(268, 71%, 12%)'
+        inps[i].style.color = 'hsl(52, 100%, 62%)'
+    }
 
-           result.innerHTML += inputs[2].value
-       }
-
-       function num4() {
-
-           result.innerHTML += inputs[4].value
-       }
-       function num5() {
-
-           result.innerHTML += inputs[5].value
-       }
-       function num6() {
-
-           result.innerHTML += inputs[6].value
-       }
-       function num1() {
-
-           result.innerHTML += inputs[8].value
-       }
-       function num2() {
-
-           result.innerHTML += inputs[9].value
-       }
-       function num3() {
-
-           result.innerHTML += inputs[10].value
-       }
-       function num0() {
-
-           result.innerHTML += inputs[13].value
-       }
+    for (i = 4; i <= 15; i++) {
+        inps[i].style.color = 'hsl(52, 100%, 62%)'
+        inps[18].style.color = 'hsl(52, 100%, 62%)'
+        inps[i].style.background = 'hsl(268, 71%, 12%)'
+        inps[3].style.background = 'hsl(281, 89%, 26%)'
+        inps[17].style.background = 'hsl(176, 100%, 44%)'
+        inps[16].style.background = 'hsl(281, 89%, 26%)'
+        inps[18].style.background = 'hsl(268, 71%, 12%)'
+        inps[17].style.color = 'black'
+    }
 
 
-       function somar() {
-           
-         
-           if(valorSoma1 <6 ) {
-           valorSoma2 = Number(result.textContent)
-               console.log(valorSoma2)
-           }
-           
-           
-           
-         
-           
-       }
-       function subtrair() {
+   
+    resultado.style.background = 'hsl(268, 71%, 12%)'
+   resultado.style.color = 'hsl(52, 100%, 62%)'
+    pads.style.background = ' hsl(268, 71%, 12%)'
+   pos1.style.background = 'hsl(224, 36%, 15%)'
+   pos2.style.background = 'hsl(224, 36%, 15%)'
+    pos3.style.background = 'hsl(176, 100%, 44%)'
+}
 
-           result.innerHTML += inputs[11].value
-       }
-       function dividir() {
 
-           result.innerHTML += inputs[14].value
-       }
-       function multiplicar() {
+pos1.addEventListener('click', tema1)
+pos2.addEventListener('click', tema2)
+pos3.addEventListener('click', tema3)
+//mudar tema// 
 
-           result.innerHTML += inputs[15].value
-       }
-       function resetar() {
 
-           result.innerHTML += inputs[16].value
-       }
-       function resultado() {
+var resultado = document.getElementById("resultado")
 
-         if (valorSoma1 && valorSoma2) {
-           console.log(valorSoma1)
-             console.log(valorSoma2)
-         }
-       }
+function calcNum(num) {
+    if(typeof resultado == 'undefinied') {
+        resultado.value = ''
+    }
+    resultado.value = resultado.value + num
+  }
 
-       function porcentagem() {
+  function deletar() {
+      resultado.value = ''
+    }
 
-           result.innerHTML += inputs[18].value
-       }
-
-       function ponto() {
-
-           result.innerHTML += inputs[12].value
-       }
-       function deleta() {
-
-           //pos4
-       }
-
-       inputs[0].addEventListener('click', num7)
-       inputs[1].addEventListener('click', num8)
-       inputs[2].addEventListener('click', num9)
-       inputs[3].addEventListener('click', deleta)
-       inputs[4].addEventListener('click', num4)
-       inputs[5].addEventListener('click', num5)
-       inputs[6].addEventListener('click', num6)
-       inputs[7].addEventListener('click', somar)
-       inputs[8].addEventListener('click', num1)
-       inputs[9].addEventListener('click', num2)
-       inputs[10].addEventListener('click', num3)
-       inputs[11].addEventListener('click', subtrair)
-       inputs[12].addEventListener('click', ponto)
-       inputs[13].addEventListener('click', num0)
-       inputs[14].addEventListener('click', dividir)
-       inputs[15].addEventListener('click', multiplicar)
-       inputs[16].addEventListener('click', resetar)
-       inputs[18].addEventListener('click', porcentagem)
-       inputs[17].addEventListener('click', resultado)
-     
-   APRENDENDO...
-*/ 
+    function soma(num1, num2) { 
+        num1 = Number(resultado.value)
+        num2 = Number(resultado.value)
+        
+        console.log(num1, num2)
+        
+     }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 			<u>Calculadora</u>
+# 			<u>Calculadora temática</u>
 
 ### Essa calculadora irá realizar cálculos de:
 
@@ -13,7 +13,9 @@
 - #### Pocentagem.
 
 
-**Será desenvolvido um modelo para mobile e outro para desktop**
+
+
+**Terá 3 opções de tema para o usuário escolher enquanto estiver usando o programa e será desenvolvido um modelo para mobile e outro para desktop**
 
 **Desktop:**
 
@@ -43,15 +45,22 @@
 
 
 
-| TASKS                                    |    PROGRESSO     | *REQUERIDO PARA* | *ENTREGUE EM* | *RELEVÂNCIA* 1/10 |
-| :--------------------------------------- | :--------------: | :--------------: | :-----------: | :---------------: |
-| - Criar esqueleto da pasta main e criar arquivos: **.hml**, **.js** , .**css**, **.md** ; |  **Concluído**   |    24/09/2021    |  24/09/2021   |       8/10        |
-| - Criar esboço da página index.html;     |  **Concluído**   |    24/09/2021    |  24/09/2021   |       8/10        |
-| - Criar um repositório e upar a pasta do projeto no GitHub; |  **Concluído**   |    24/09/2021    |  24/09/2021   |       10/10       |
-| -                                        | **Em andamento** |                  |               |                   |
-|                                          | **Em andamento** |                  |               |                   |
-|                                          | **Em andamento** |                  |               |                   |
-|                                          | **Em andamento** |                  |               |                   |
-|                                          | **Em andamento** |                  |               |                   |
-|                                          | **Em andamento** |                  |               |                   |
+| ---------------------------TASKS---------------------- |    PROGRESSO     | *REQUERIDO PARA* |    *ENTREGUE EM*    | *RELEVÂNCIA* 1/5 |
+| :--------------------------------------- | :--------------: | :--------------: | :-----------------: | :--------------: |
+| - Criar esqueleto da pasta main e criar arquivos: **.hml**, **.js** , .**css**, **.md** ; |  **Concluído**   |    24/09/2021    |     24/09/2021      |       3/5        |
+| - Criar esboço da página index.html;     |  **Concluído**   |    24/09/2021    |     24/09/2021      |       4/5        |
+| - Criar um repositório e upar a pasta do projeto no GitHub; |  **Concluído**   |    24/09/2021    |     24/09/2021      |       4/5        |
+| - Adiocionar um seletor de 3 opções de tema; |  **Concluído**   |    25/09/2021    |     25/09/2021      |       3/5        |
+| - Criar 3 temas diferentes e ativar de acordo com o que o usuário escolher (Eventos em JavaScript); |  **Concluído**   |    25/09/2021    |     25/09/2021      |       3/5        |
+| -  Criar modelo responsivo para mobile.  | **Em andamento** |    27/09/2021    | **Dentro do prazo** |       3/5        |
+| - Desenvolver a parte lógica da calculadora; | **Em andamento** |    30/09/2021    | **Dentro do prazo** |       5/5        |
+| - Revisão do projeto;                    | **Em andamento** |    01/09/2021    | **Dentro do prazo** |       4/5        |
+| - Testes e correções de erros;           | **Em andamento** |    01/09/2021    | **Dentro do prazo** |       4/5        |
+| - Entrega do projeto.                    | **Em andamento** |    02/09/2021    | **Dentro do prazo** |       5/5        |
 
+## Técnologias requisitadas para o projeto:
+
+- JavaScript;
+- CSS3;
+- HTML5;
+- BOOTSTRAP.
